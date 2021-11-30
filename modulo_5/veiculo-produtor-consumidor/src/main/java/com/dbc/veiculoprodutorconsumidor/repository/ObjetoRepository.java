0198aac30b3266a2da239a16537d4b465536e63d
Leo-kardo <leonardo_c_g@hotmail.com>
2021-11-30 20:21:47 -0300

    melhoramentos

--- modulo_5/veiculo-produtor-consumidor/src/main/java/com/dbc/veiculoprodutorconsumidor/repository/ObjetoRepository.java
+++ modulo_5/veiculo-produtor-consumidor/src/main/java/com/dbc/veiculoprodutorconsumidor/repository/ObjetoRepository.java
@@ -4,4 +4,5 @@ import com.dbc.veiculoprodutorconsumidor.entity.ObjetoEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ObjetoRepository extends MongoRepository<ObjetoEntity, String> {
+
 }
