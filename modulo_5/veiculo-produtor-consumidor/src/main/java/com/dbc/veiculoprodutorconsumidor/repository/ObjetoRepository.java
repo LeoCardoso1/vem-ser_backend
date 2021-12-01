38e47b801c3c804ab0a4bb9cccd1ba6ab68a2b88
Leo-kardo <leonardo_c_g@hotmail.com>
2021-12-01 07:49:36 -0300

    melhoramentos

--- modulo_5/veiculo-produtor-consumidor/src/main/java/com/dbc/veiculoprodutorconsumidor/repository/ObjetoRepository.java
+++ modulo_5/veiculo-produtor-consumidor/src/main/java/com/dbc/veiculoprodutorconsumidor/repository/ObjetoRepository.java
@@ -2,7 +2,11 @@ package com.dbc.veiculoprodutorconsumidor.repository;
 
 import com.dbc.veiculoprodutorconsumidor.entity.ObjetoEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface ObjetoRepository extends MongoRepository<ObjetoEntity, String> {
+
+    @Query(value = "{status: ?0}", count = true)
+    long count(boolean status);
 
 }
