8bea622ea40bbd64fe238e33935979cb0fd28c2d
Leo-kardo <leonardo_c_g@hotmail.com>
2021-12-02 14:52:43 -0300

    ajustes

--- modulo_5/Schedule/pessoaapi/src/main/java/com/dbc/pessoaapi/scheduler/Rotinas.java
+++ modulo_5/Schedule/pessoaapi/src/main/java/com/dbc/pessoaapi/scheduler/Rotinas.java
@@ -1,10 +1,7 @@
 package com.dbc.pessoaapi.scheduler;
 
-import com.dbc.pessoaapi.service.EmailService;
 import com.dbc.pessoaapi.service.PessoaService;
 import freemarker.template.TemplateException;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,15 +14,19 @@ import java.text.SimpleDateFormat;
 @RequiredArgsConstructor
 @Component
 @Slf4j
-public class RotinaUsuarioNatal {
-
+public class Rotinas {
     private static final SimpleDateFormat dateFormat
             = new SimpleDateFormat("HH:mm:ss");
 
     private final PessoaService pessoaService;
 
-    @Scheduled(cron = "* * * 23 12 ?", zone = "GMT-3")
+    @Scheduled(cron = "0 0 8,20 * * *", zone = "GMT-3")
     public void emailSemEndereco() throws InterruptedException, MessagingException, TemplateException, IOException {
+        pessoaService.sendPessoaSemEndereco();
+    }
+
+    @Scheduled(cron = "* * * 23 12 ?", zone = "GMT-3")
+    public void emailNatal() throws InterruptedException, MessagingException, TemplateException, IOException {
         pessoaService.sendEmailNatal();
     }
 }
